4b4bb3923f6c8debe19a74192470646948710de8
Morian Senador <smorian@yahoo.com>
2021-10-28 11:08:38 -0400

    wip @ navbarstyles.js

--- src/components/navbar/NavbarStyles.js
+++ src/components/navbar/NavbarStyles.js
@@ -2,15 +2,66 @@ import styled from "styled-components";
 import { Container } from "../../globalStyles";
 import { Link } from "react-router-dom";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  background: transparent;
+  margin-bottom: -80px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  position: absolute;
+  top: 0;
+  z-index: 50;
+  width: 100%;
+  transition: background-color 0.3s ease-in;
+`;
 
-export const NavbarContainer = styled(Container)``;
+export const NavbarContainer = styled(Container)`
+  display: flex;
+  justify-content: start;
+  height: 80px;
+  ${Container}
+`;
 
-export const NavLogo = styled(Link)``;
+export const NavLogo = styled(Link)`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  z-index: 50;
+`;
 
 export const NavIcon = styled.img``;
 
 export const MobileIcon = styled.div``;
-export const = NavMenu,
-export const = NavLinks,
-export const = NavItem,
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  width: 100%;
+
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    padding-top: 30%;
+    top: 0;
+    left: 0;
+    opacity: ${({ show }) => (show ? 1 : 0)};
+    visibility: ${({ show }) => (show ? "visible" : "hidden")};
+    transform: translateY(${({ show }) => (show ? "0" : "-10px")});
+    transition: opacity 0.5s ease;
+    background-color: #071c2f;
+  }
+`;
+
+export const NavLinks = styled.span``;
+
+export const NavItem = styled.li``;
