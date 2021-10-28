09402f0be9567d92ace0f2e4477da068d7801c60
Morian Senador <smorian@yahoo.com>
2021-10-28 10:04:52 -0400

    wip @ NavbarStyles.js

---
+++ src/components/navbar/NavbarStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Container } from "../../globalStyles";
+import { Link } from "react-router-dom";
+
+export const Nav = styled.nav``;
+
+export const NavbarContainer = styled(Container)``;
+
+export const NavLogo = styled(Link)``;
+
+export const NavIcon = styled.img``;
+
+export const MobileIcon = styled.div``;
+export const = NavMenu,
+export const = NavLinks,
+export const = NavItem,
